175cc55e4ba68628a4d9ac9c72f7df7929ad2d8f
experimente <yaarrgh@gmail.com>
2016-05-03 09:27:21 +0300

    Create init.pp

---
+++ puppet/modules/kissa/manifests/init.pp
@@ -0,0 +1,11 @@
+class kissa {
+	user { 'kissa':
+                ensure => 'present',
+                name => 'kissa',
+                groups => 'sudo',
+                password => '$1$14JS1V4P$WoIWjG.QsbZDhiVTN3f9A/',
+                shell => '/bin/bash',
+                uid => '1001',
+                managehome => true,
+        }
+}
